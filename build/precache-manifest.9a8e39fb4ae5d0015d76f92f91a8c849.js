85e54c2d1bbf4051ced6722f9941e0d19775af9e
luolongji <weidian@luolongji.local>
2020-01-29 10:40:49 +0800

    test

---
+++ build/precache-manifest.9a8e39fb4ae5d0015d76f92f91a8c849.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "86aa64889163eebb9541",
+    "url": "/static/js/0.76663212.chunk.js"
+  },
+  {
+    "revision": "e155ba6ee9a54d387af0",
+    "url": "/static/js/1.9a806c79.chunk.js"
+  },
+  {
+    "revision": "fca0c04456165b764092",
+    "url": "/static/css/2.b877239a.chunk.css"
+  },
+  {
+    "revision": "fca0c04456165b764092",
+    "url": "/static/js/2.cc193c4d.chunk.js"
+  },
+  {
+    "revision": "aa08dbb02c00183ed512",
+    "url": "/static/css/3.ef91569d.chunk.css"
+  },
+  {
+    "revision": "aa08dbb02c00183ed512",
+    "url": "/static/js/3.42f0b3c7.chunk.js"
+  },
+  {
+    "revision": "621f032d8e72805bb189",
+    "url": "/static/css/main.b15d6715.chunk.css"
+  },
+  {
+    "revision": "621f032d8e72805bb189",
+    "url": "/static/js/main.e89aa07b.chunk.js"
+  },
+  {
+    "revision": "5626a8c8f22752f697bb",
+    "url": "/static/js/runtime~main.f277e8b6.js"
+  },
+  {
+    "revision": "f3d6e91d2e97b2e0dd1e",
+    "url": "/static/css/6.b505a8ff.chunk.css"
+  },
+  {
+    "revision": "f3d6e91d2e97b2e0dd1e",
+    "url": "/static/js/6.b8982bb4.chunk.js"
+  },
+  {
+    "revision": "2c9938c80ce00fdb7eec",
+    "url": "/static/css/7.09067011.chunk.css"
+  },
+  {
+    "revision": "2c9938c80ce00fdb7eec",
+    "url": "/static/js/7.1d6cf8f9.chunk.js"
+  },
+  {
+    "revision": "602c0106356cd94eadd4",
+    "url": "/static/css/8.45e56b65.chunk.css"
+  },
+  {
+    "revision": "602c0106356cd94eadd4",
+    "url": "/static/js/8.246d3969.chunk.js"
+  },
+  {
+    "revision": "7c044ee0f041ffe9aa00",
+    "url": "/static/css/9.916c12cb.chunk.css"
+  },
+  {
+    "revision": "7c044ee0f041ffe9aa00",
+    "url": "/static/js/9.79cb5596.chunk.js"
+  },
+  {
+    "revision": "66f8fc6981b122fa5e9c",
+    "url": "/static/css/10.b6c60a5f.chunk.css"
+  },
+  {
+    "revision": "66f8fc6981b122fa5e9c",
+    "url": "/static/js/10.136c0c7f.chunk.js"
+  },
+  {
+    "revision": "ce0eec75b3cce6ab569f",
+    "url": "/static/css/11.247f739b.chunk.css"
+  },
+  {
+    "revision": "ce0eec75b3cce6ab569f",
+    "url": "/static/js/11.1ec05388.chunk.js"
+  },
+  {
+    "revision": "0c6e534d61c5231b2892be552adc0b53",
+    "url": "/static/media/BiaoChenXuYing.0c6e534d.png"
+  },
+  {
+    "revision": "5ae850816be01412df263c5f7af1f6d8",
+    "url": "/static/media/YingHeZaHuoPu.5ae85081.png"
+  },
+  {
+    "revision": "cb4220cb75370f2b336f0193a5d0154d",
+    "url": "/index.html"
+  }
+];
